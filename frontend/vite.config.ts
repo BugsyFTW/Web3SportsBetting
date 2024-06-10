645e420e67e80483f61db4c6b711681b3962bd49
BugsyFTW <bugsyftw@gmail.com>
2024-06-10 22:41:54 +0100

    added ui.shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8080
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "./src"),
+      /*'@components': '/src/components',
+      '@config': '/src/config',
+      '@hooks': '/src/hooks',
+      '@lib': '/src/lib',
+      '@utils': '/src/utils',
+      types: '/src/types' */
+    }
   }
 })
